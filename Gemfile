6e0dbe9c436fa2483b4a2ba6261905f8e8358e30
misterbao <mister.bao@gmail.com>
2019-06-02 16:55:37 +0000

    ticker symbol search bar

--- Gemfile
+++ Gemfile
@@ -62,3 +62,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap', '~> 4.3', '>= 4.3.1'
 gem 'jquery-rails'
+gem 'stock_quote', '~> 2.0'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
   def index
+    if params[:ticker] == ""
+      @nothing = "You Forgot to Enter a Ticker Symbol"
+    elsif params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      if !@stock
+          @error = "That Ticker Symbol Does Not Exist, Please Try Another"
+      end
+    end
+
   end
 
   def about
   end
+
 end
